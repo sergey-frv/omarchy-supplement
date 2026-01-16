94bc12bbea7a937de82741c1eb0082274d03845c
Sergey Frolov <sergey.frv@yandex.ru>
2026-01-17 01:36:30 +0600

    openfortivpn tui install

--- install-openfortivpn.sh
+++ install-openfortivpn.sh
@@ -2,3 +2,13 @@
 
 # Install forti client vpn
 yay -S --noconfirm --needed openfortivpn
+
+CURRENT_DIR=$(pwd)
+APP_NAME="OpenFrotiVPN"
+APP_ICON_URL="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/fortinet.png"
+
+sudo rm -rf /usr/bin/openfortivpn-tui
+sudo ln -s "$CURRENT_DIR/openfortivpn-tui" "/usr/bin/openfortivpn-tui"
+
+omarchy-tui-remove $APP_NAME
+omarchy-tui-install $APP_NAME openfortivpn-tui float $APP_ICON_URL
